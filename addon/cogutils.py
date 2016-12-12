035e27e2e464b4bcf34a7bda76df507dc15cc41c
Jerome Boisvert-Chouinard <jerome.boisvertchouinard@savoirfairelinux.com>
2016-12-12 17:09:13 -0500

    add common module categories

--- addon/cogutils.py
+++ addon/cogutils.py
@@ -1,17 +1,40 @@
 # -*- coding: utf-8 -*-
+
 TITLE='Module Title'
+
+# COMMON MODULE CATEGORIES
+# Customer Relationship Management
+# Sales Management
+# Project Management
+# Knowledge Management
+# Warehouse Management
+# Manufacturing
+# Invoicing & Payments
+# Accounting & Finance
+# Purchase Management
+# Human Resources
+# Extra Tools
+# Marketing
+# Point of Sale
+# Advanced Reporting
 CATEGORY='Category'
+
 SUMMARY='One line summary of module function.'
+
 DESCRIPTION="""
 This is a longer description of the module.
 
 It can span multiple lines.
 """
 ODOO_VERSION='9.0'
+
+
 CONTRIBUTORS=[
     ('Jérome Boisvert-Chouinard',
      'jerome.boisvertchouinard@savoirfairelinux.com'),
 ]
+
 LICENSE="AGPL-3.0"
+
 LICENSE_HEADER="""# © 2016 Savoir-faire Linux
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)."""
